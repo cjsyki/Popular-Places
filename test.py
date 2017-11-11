5237dfbfe1d738db57a8f7a5c6af6dbf983c3223
CJ Syki <clevin.syki@gmail.com>
2017-11-11 01:53:43 -0500

    import/export text file

--- test.py
+++ test.py
@@ -5,20 +5,23 @@ temp = open( "places.txt", "r" );
 text = temp.read( ).split( "id: ");
 temp.close( );
 
-for line in text:
-    print( line );
-# bigList = populartimes.get("AIzaSyBo8toIXI4IvGZdvvpRtW9Pgf23ub1sVIg", ["bar"], (40.733400, -73.995484), (40.735847, -73.993542))
 
-# def printTimes( ):
-#     for dicti in bigList:
-#         print( "id: " + str( dicti[ "id" ] ) );
-#         data = "";
-#         for days in dicti[ "populartimes" ]:
-#             data += days[ "name" ] + ": ";
-#             for hours in days[ "data" ];
-#                 data += str( hours ) + " ";
-#             print( data );
-#             data = "";
+if len( text ) == 0:
+    temp = open( "places.txt", "w" );
+    bigList = populartimes.get("AIzaSyBo8toIXI4IvGZdvvpRtW9Pgf23ub1sVIg", ["bar"], (40.733400, -73.995484), (40.735847, -73.993542))
+    for dicti in bigList:
+        data = "id: " + str( dicti[ "id" ] );
+        for days in dicti[ "populartimes" ]:
+            data += days[ "name" ] + ": ";
+            for hours in days[ "data" ]:
+                data += str( hours ) + " ";
+            print( data );
+            temp.write( data );
+    temp.close( );
+else:
+    for line in text:
+        print( line );
+    
     
 
 # printTimes( );

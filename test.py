ca33331f5b179efffeb4e6a4d841ff08d09f0e6b
CJ Syki <clevin.syki@gmail.com>
2017-11-11 02:08:08 -0500

    fixed importing text issue

--- test.py
+++ test.py
@@ -1,27 +1,24 @@
 import populartimes;
 
-
 temp = open( "places.txt", "r" );
 text = temp.read( ).split( "id: ");
 temp.close( );
 
 
-if len( text ) == 0:
+if len( text ) == 1:
     temp = open( "places.txt", "w" );
     bigList = populartimes.get("AIzaSyBo8toIXI4IvGZdvvpRtW9Pgf23ub1sVIg", ["bar"], (40.733400, -73.995484), (40.735847, -73.993542))
     for dicti in bigList:
-        data = "id: " + str( dicti[ "id" ] );
+        data = "id: " + str( dicti[ "id" ] ) + "\n" ;
         for days in dicti[ "populartimes" ]:
             data += days[ "name" ] + ": ";
             for hours in days[ "data" ]:
                 data += str( hours ) + " ";
-            print( data );
-            temp.write( data );
+            data += "\n"
+        print( data + "\n" );
+        temp.write( data + "\n" );
     temp.close( );
 else:
     for line in text:
         print( line );
     
-    
-
-# printTimes( );

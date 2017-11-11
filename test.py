5c126d9a62c7467cb1a3308b2322344c894030c7
CJ Syki <clevin.syki@gmail.com>
2017-11-11 03:30:11 -0500

    works!
    inputs coordinates, code creates boundaries and works

--- test.py
+++ test.py
@@ -7,20 +7,20 @@ temp.close( );
 placeID = "";
 currentHour = "";
 currentDay = "";
-
+closedString = "No places are open"
 #(40.733400, -73.995484), (40.735847, -73.993542)
 
 # creates a dictionary of places within a box, makes a new dictionary with all the places along w their popularity at hour hour, and returns best place
-def findBestPlace( hour, day, south_lat, south_long, north_lat, north_long ):
+def findBestPlace( hour, day, lat, long ):
     #create big dictionary using api
-    bigList = populartimes.get("AIzaSyBo8toIXI4IvGZdvvpRtW9Pgf23ub1sVIg", ["bar"], ( 40.733400, -73.995484 ), ( 40.735847, -73.993542 ) )
+    bigList = populartimes.get("AIzaSyBo8toIXI4IvGZdvvpRtW9Pgf23ub1sVIg", ["bar"], ( lat - 00.002500, long - 00.001000 ), ( lat + 00.002500, long + 00.001000 ) )
     newDict = { };      #new dictionary
     for dicti in bigList:       # for each element in the big dictinary
-        id = str( dicti[ "id" ] )       #store id of the location temporarily
+        name = str( dicti[ "name" ] )       #store id of the location temporarily
         for days in dicti[ "populartimes" ]:    #for each day in the populartimes key
             if days[ "name" ] != day:   # if you arent at the current day, continue to next day
                 continue;
-            newDict[ id ] = days[ "data" ][ hour ];  #store places id as key and places populartiy at hour hour as value
+            newDict[ name ] = days[ "data" ][ hour ];  #store places id as key and places populartiy at hour hour as value
             break;
     print( newDict ); #test print
     print( lowestValue( newDict ) );
@@ -30,9 +30,11 @@ def lowestValue( dictionary ):
     minKey = ""
     minVal = 101;
     for key, value in dictionary.items( ):
-        if value < minVal:
+        if value < minVal and value != 0:           #does not equal zero bc ITS CLOSED!!!
             minVal = value;
             minKey = key;
-    return key
+    if minKey == "":
+        return closedString;
+    return minKey
 
-findBestPlace( 23, "Friday" );
+findBestPlace( 23, "Friday", 40.733441, -73.995186 );
